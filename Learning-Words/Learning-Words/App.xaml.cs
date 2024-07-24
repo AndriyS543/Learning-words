7f4473394db69e08c8d4f6477297deeba4d848bf
Andriy Sozanskiy <sozanskiyandriyw@gmail.com>
2024-07-24 23:33:18 +0300

    Initial project setup

---
+++ Learning-Words/Learning-Words/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Learning_Words
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
