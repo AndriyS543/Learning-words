aa303e03a14b26fb235fab8ab00bf18cf975e40c
Andriy Sozanskiy <sozanskiyandriyj@gmail.com>
2024-08-21 02:56:06 +0300

    Add TestResult

---
+++ Learning-Words/Learning-Words/Models/TestResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_Words.Models
+{
+    public class TestResult
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime TestDate { get; set; }
+        public int CorrectAnswers { get; set; }
+        public int Mistakes { get; set; }
+        public List<Word> Words { get; set; } = new List<Word>();
+    }
+
+}
