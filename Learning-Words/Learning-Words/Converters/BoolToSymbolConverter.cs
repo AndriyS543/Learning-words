a61f5a84b14a01d0defcd05d9e6e243afc6343e7
Andriy Sozanskiy <sozanskiyandriyj@gmail.com>
2024-08-21 02:52:44 +0300

    Implement Test UI

---
+++ Learning-Words/Learning-Words/Converters/BoolToSymbolConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Learning_Words.Converters
+{
+    public class BoolToSymbolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? "➖" : "✔";
+            }
+            return "➖";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
