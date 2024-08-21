6a81a1bfdd065f8cdbd09ee58942c6bd3c719758
Andriy Sozanskiy <sozanskiyandriyj@gmail.com>
2024-08-21 03:08:34 +0300

    Implement color conversion for correct answers in test results

---
+++ Learning-Words/Learning-Words/Converters/CorrectAnswerToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Learning_Words.Converters
+{
+
+    public class CorrectAnswerToColorConverter : IValueConverter
+    {
+       public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+{
+    if (value is bool isCorrect)
+    {
+        return isCorrect ? (Brush)new SolidColorBrush((Color)ColorConverter.ConvertFromString("#00481C")) // Green
+                         : (Brush)new SolidColorBrush((Color)ColorConverter.ConvertFromString("#640007")); // Red
+    }
+    return Brushes.Transparent;
+}
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
